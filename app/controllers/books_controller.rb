0cfe33166faa56683580b80b32d504baadb1843f
EC2 Default User <ec2-user@ip-172-31-25-136.ec2.internal>
2022-12-23 05:43:59 +0000

    bookのindex,showページとcreate機能の作成、userのindexページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,27 @@
 class BooksController < ApplicationController
-  def new
-  end
-
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def index
+    @book = Book.new(book_params)
+    @books = Book.all
+    @user = current_user
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = current_user
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    @book = Book.new
+    @user = current_user
+    @users = User.all
+  end
+
   def show
+    @user = User.find(params[:id])
+    @books = Book.all
+    @book = Book.new
   end
 
   def edit
