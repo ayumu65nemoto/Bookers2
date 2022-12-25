2051374a74ee6308448b4ecaa501597ed0c20880
EC2 Default User <ec2-user@ip-172-31-25-136.ec2.internal>
2022-12-25 17:06:40 +0000

    Final Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
     unless @user == current_user
-      redirect_to books_path
+      redirect_to user_path(current_user)
     end
   end
 
